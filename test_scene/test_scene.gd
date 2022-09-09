e7381db946467d33080dcd84da876dc1e6c2b302
Kretus <kretus9999@gmail.com>
2022-09-09 18:54:52 -0400

    TEST

--- test_scene/test_scene.gd
+++ test_scene/test_scene.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("TEST TEST OH OOH OOH AH AH")
+	print("TEST TEST OH OOH OOH AH AH MONKEY MODE")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
